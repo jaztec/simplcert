0c9627a43337aa7f23d58a2ccb8bb58e97411136
Jasper van Herpt <jasper.v.herpt@gmail.com>
2023-02-12 00:05:38 +0100

    Get server part up and running

--- examples/rust-server-client/apps/server/src/main.rs
+++ examples/rust-server-client/apps/server/src/main.rs
@@ -1,11 +1,31 @@
+use eyre::Result;
+use tonic::transport::Server;
+use proto::greeter::{GreetingResponse, GreetingRequest};
+use proto::greeter::greeter_service_server::GreeterServiceServer;
+
 #[derive(Default, Debug)]
 pub struct GreetingService;
 
-#[tonic_async_trait]
+#[tonic::async_trait]
 impl proto::greeter::greeter_service_server::GreeterService for GreetingService {
-    async fn greet()
+    async fn greet(
+        &self,
+        request: tonic::Request<GreetingRequest>,
+    ) -> Result<tonic::Response<GreetingResponse>, tonic::Status> {
+        Ok(
+            tonic::Response::new(GreetingResponse { greeting: format!("Hello {}", request.into_inner().name) })
+        )
+    }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let service = GreeterServiceServer::new(GreetingService{});
+
+    Server::builder()
+        .add_service(service)
+        .serve(":8000".parse().unwrap())
+        .await?;
+
+    Ok(())
 }
