8c79dc789787116d44cb3127845ea1a3512ce9d8
Jasper van Herpt <jasper.v.herpt@gmail.com>
2023-02-11 23:42:36 +0100

    Add some Docker scaffolding

--- examples/rust-server-client/apps/server/src/main.rs
+++ examples/rust-server-client/apps/server/src/main.rs
@@ -1,3 +1,11 @@
+#[derive(Default, Debug)]
+pub struct GreetingService;
+
+#[tonic_async_trait]
+impl proto::greeter::greeter_service_server::GreeterService for GreetingService {
+    async fn greet()
+}
+
 fn main() {
     println!("Hello, world!");
 }
