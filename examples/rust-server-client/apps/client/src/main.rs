35ad75bea26d04dea5f1284486e8aa0f3f7de281
Jasper van Herpt <jasper.v.herpt@gmail.com>
2023-02-12 00:26:01 +0100

    Implement the client for basics

--- examples/rust-server-client/apps/client/src/main.rs
+++ examples/rust-server-client/apps/client/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::Result;
+use proto::greeter::{greeter_service_client::GreeterServiceClient, GreetingRequest};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = GreeterServiceClient::connect("server:8000").await?;
+
+    let req = tonic::Request::new(GreetingRequest {
+        name: "World".into_string(),
+    });
+
+    let resp = client.greet(req).await?;
+    println!("Hello {}", resp.into_inner().greeting);
+
+    Ok(())
 }
