2c804ab9144a12b14d283d582a81c16b6e309b07
Jasper van Herpt <jasper.v.herpt@gmail.com>
2023-02-12 13:52:24 +0100

    Add client code for mTLS gRPC connection

--- examples/rust-server-client/apps/client/src/main.rs
+++ examples/rust-server-client/apps/client/src/main.rs
@@ -1,9 +1,19 @@
 use eyre::Result;
+use tonic::transport::{Channel, ClientTlsConfig};
 use proto::greeter::{greeter_service_client::GreeterServiceClient, GreetingRequest};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut client = GreeterServiceClient::connect("server:8000").await?;
+    let config = ClientTlsConfig::new()
+        .ca_certificate(common::load_root_cert())
+        .domain_name("server")
+        .identity(common::load_identity("client").unwrap());
+    let channel = Channel::from_static("server:8000")
+        .tls_config(config)?
+        .connect()
+        .await?;
+
+    let mut client = GreeterServiceClient::new(channel);
 
     let req = tonic::Request::new(GreetingRequest {
         name: "World".to_string(),
